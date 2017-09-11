d32bafd6564a0353efb87beb0b6de9df63f0fb1a
Alexander Chuev <AlexanderChuev@coherentsolutions.com>
2017-09-11 19:08:25 +0300

    add Gateway API

---
+++ reservation-client/src/main/java/com/example/reservationclient/Reservation.java
@@ -0,0 +1,10 @@
+package com.example.reservationclient;
+
+import lombok.Data;
+
+@Data
+public class Reservation {
+    private Long id;
+    private String reservationName;
+
+}
---
+++ reservation-client/src/main/java/com/example/reservationclient/ReservationClientApplication.java
@@ -0,0 +1,27 @@
+package com.example.reservationclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ReservationClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationClientApplication.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+}
